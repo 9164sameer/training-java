665db772caadf09b7502cbd1373903cb33402629
9164sameer <117854143+9164sameer@users.noreply.github.com>
2023-03-27 10:28:57 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,11 @@
+class Program1
+{
+public static void main(String args[]){
+	int num=5;
+	int num2=7;
+	int res=num1+num2;
+System.out.println(num1+"+"+num2+"="+res);
+res=num1-num2;
+System.out.println(num1+"-"+num2+"="+res);
+}//main()
+}//Program1
